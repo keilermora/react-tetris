c5971fafd76f654a802e7aeb5a995f6551ee3d6b
Keiler Mora <keilermora@gmail.com>
2022-07-14 12:33:31 -0400

    ✨ feat: show next tetrimino shape in next queue component

--- src/game/components/NextQueue/NextQueue.tsx
+++ src/game/components/NextQueue/NextQueue.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
+import { useGameState } from '../../../hooks/useGameState';
 import { MatrixCell } from '../MatrixCell';
 import './NextQueue.css';
 
 const NextQueue = () => {
-  // @TODO Draw a real Tetrimino
-  const tetrimino = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-  ];
+  const { state } = useGameState();
+  const { nextTetrimino } = state;
 
-  const nextQueueGrid = tetrimino.map((rowArray, row) => {
+  const tetriminoShape = nextTetrimino.shapes[0];
+
+  const nextQueueGrid = tetriminoShape.map((rowArray, row) => {
     return rowArray.map((value, col) => {
-      return <MatrixCell key={`${row}${col}`} colorNumber={value} />;
+      return (
+        <MatrixCell
+          key={`${row}${col}`}
+          colorNumber={value ? nextTetrimino.colorNumber : 0}
+        />
+      );
     });
   });
 
